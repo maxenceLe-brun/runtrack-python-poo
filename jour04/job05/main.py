176ed9e5cb85e26dceb9b6f2e4ee7662a496a7d6
Maxence Le Brun <115214683+maxenceLe-brun@users.noreply.github.com>
2023-03-07 23:18:18 +0100

    Create main.py

---
+++ jour04/job05/main.py
@@ -0,0 +1,16 @@
+import math
+class Forme:
+    def __init__(self):pass
+    def aire(self):return 0
+class Rectangle(Forme):
+    def __init__(self, largeur, hauteur):
+        self.largeur, self.hauteur = largeur, hauteur
+    def aire(self):return self.largeur * self.hauteur
+class Cercle(Forme):
+    def __init__(self, radius):self.radius = radius
+    def aire(self):return math.pi * self.radius ** 2
+
+rectangle = Rectangle(5, 7)
+print("Aire du rectangle : {}".format(rectangle.aire()))
+cercle = Cercle(3)
+print("Aire du cercle : {}".format(cercle.aire()))
